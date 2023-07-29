aba0e494f4edba9f3ae5664fe823575f2449dcd9
trancongtien <tientc.21it@vku.udn.vn>
2023-07-30 00:02:26 +0700

    t

--- index.js
+++ index.js
@@ -41,14 +41,13 @@ app.get("/", function (req, res) {
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
-      "Access-Control-Allow-Headers",
-      "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+    "Access-Control-Allow-Headers",
+    "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
   );
   res.header("Access-Control-Allow-Methods", "GET, POST");
   res.header("Allow", "GET, POST");
   next();
 });
-
 
 app.get("/users/login", function (req, res) {
   res.render("pages/dashboard");
